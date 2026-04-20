28473e6c767e3067aea6bca81907ddeed5dda89e
VEERA MANI <veeramanikandan@veeras-MacBook-Air.local>
2026-04-20 21:39:51 +0530

    Initial commit - AI Mock Interview Project

---
+++ client/src/App.jsx
@@ -0,0 +1,49 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import Navbar from './components/Navbar';
+import ProtectedRoute from './components/ProtectedRoute';
+import HomePage from './pages/HomePage';
+import InterviewSetupPage from './pages/InterviewSetupPage';
+import InterviewPage from './pages/InterviewPage';
+
+function App() {
+  return (
+    <div className="app-shell">
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Navbar />
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/setup"
+          element={
+            <ProtectedRoute>
+              <Navbar />
+              <InterviewSetupPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/interview/:id"
+          element={
+            <ProtectedRoute>
+              <Navbar />
+              <InterviewPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
